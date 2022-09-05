9fbcd84fc11f5c30fe2e84bea804bf46ac349d0f
sujin11-kim <su091876@gmail.com>
2022-09-05 15:12:03 +0900

    사이드 메뉴 주문받기

--- resource/order_nlp.py
+++ resource/order_nlp.py
@@ -31,11 +31,12 @@ opt_dict = data
 
 # api 1 :메뉴 추가 함수
 
+
 def add_menu(sentence):
     try:
         result_dict = {"order_list": [], "code": ""}
 
-        if sentence == "아니오" or sentence == "다음":
+        if sentence == "아니" or sentence == "다음":
             result_dict["code"] = 2001
         elif sentence == "":
             result_dict["code"] = 1002
@@ -59,6 +60,13 @@ def add_menu(sentence):
                     elif "세트" in temp_string and len(temp_string) != 2:
                         result_dict["order_list"][-1]["set"] = [201, 301]
                         temp_string = temp_string.replace("세트", "")
+                    elif temp_string == "라지":
+                        menu_temp_array = []
+
+                        for menu in result_dict["order_list"][-1]["menu"]:
+                            if "라지" in menu_dict[str(menu)]:
+                                menu_temp_array.append(menu)
+                        result_dict["order_list"][-1]["menu"] = menu_temp_array
 
                 elif word[1] == "NR":
                     count_num = int(
@@ -127,69 +135,69 @@ def select_option(sentence):
 # API NO.4 세트 메뉴 선택
 # 선택을 안할 경우 2006, 세트 선택
 def set_check(sentence, set):
-    my_set={"set":set, "code":{}}
-    temp=[]
-    a=0
-    b=0
+    my_set = {"set": set, "code": {}}
+    temp = []
+    a = 0
+    b = 0
     for v in tagger.morphs(sentence):
-        if(v =='아니' or v=='다음'):
-            my_set["code"]=2006
-    for k ,v in menu_dict.items(): 
+        if(v == '아니' or v == '다음'):
+            my_set["code"] = 2006
+    for k, v in menu_dict.items():
         if v in sentence:
-            # print(k,v) 
+            # print(k,v)
             if int(k) < 200:
                 continue
             elif int(k) < 300:
                 temp.append(int(k))
             else:
                 temp.append(int(k))
-    #사이드 메뉴와 음료의 갯수의 입력을 확인한다.    
+    # 사이드 메뉴와 음료의 갯수의 입력을 확인한다.
     for i in temp:
-        if i//100 == 2:#사이드라면
-            a+=1
-        elif i//100 == 3:#음료라면
-            b+=1
-    if(a>=2 or b>=2):#사이드나 음료가 두개이상 입력이 되면
-        my_set["code"]=2007
-    elif (my_set["code"]!=2006 and a+b>=1): 
-        my_set["code"]=2005
+        if i//100 == 2:  # 사이드라면
+            a += 1
+        elif i//100 == 3:  # 음료라면
+            b += 1
+    if(a >= 2 or b >= 2):  # 사이드나 음료가 두개이상 입력이 되면
+        my_set["code"] = 2007
+    elif (my_set["code"] != 2006 and a+b >= 1):
+        my_set["code"] = 2005
         for i in temp:
-            if i//100 ==2:
-                my_set["set"][0]=i
-            elif i//100 ==3:
-                my_set["set"][1]=i
+            if i//100 == 2:
+                my_set["set"][0] = i
+            elif i//100 == 3:
+                my_set["set"][1] = i
 
     else:
-        my_set["code"]=1002
+        my_set["code"] = 1002
 
     return my_set
-    #print(my_set)
+    # print(my_set)
 
 
-# API NO.5 
+# API NO.5
 def confirm(sentence):
-    confirm_code={"code":{}}
+    confirm_code = {"code": {}}
     for v in tagger.morphs(sentence):
-        if(v=='네' or v=='맞'or v=='넹' or v=='넵'or v=='확인'): #긍정표현
-            confirm_code["code"]=2008
-        if(v=='아니'or v=='달라요' or v=='다릅니다' or v=='엥'or v=='아닌데요'or v=='아님'): # 부정표현
-            confirm_code["code"]="추후 수정기능 구현 후 구현 예정"    
-   
-    if type(confirm_code["code"]) is dict: #분석 실패
-        confirm_code["code"]=1002  
-    #print(confirm_code)
+        if(v == '네' or v == '맞' or v == '넹' or v == '넵' or v == '확인'):  # 긍정표현
+            confirm_code["code"] = 2008
+        if(v == '아니' or v == '달라요' or v == '다릅니다' or v == '엥' or v == '아닌데요' or v == '아님'):  # 부정표현
+            confirm_code["code"] = "추후 수정기능 구현 후 구현 예정"
+
+    if type(confirm_code["code"]) is dict:  # 분석 실패
+        confirm_code["code"] = 1002
+    # print(confirm_code)
     return confirm_code
 
-agger.pos(sentence))
 
-#def main():
-    #sentence = input("sentence > ")
-    #print(tagger.pos(sentence))
+def main():
+    sentence = input("sentence > ")
+    print(tagger.pos(sentence))
     #confilct_list = [106, 107, 108]
-    #print(add_menu(sentence))
+    print(add_menu(sentence))
     #print(conflict_menu_select(sentence, confilct_list))
     # select_option(sentence)
-    #print(set_check(sentence))
-    #print(confirm(sentence))
+    # print(set_check(sentence))
+    # print(confirm(sentence))
 
-#main()
+
+main()
